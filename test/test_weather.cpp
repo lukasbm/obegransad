ad0cd2d0a367cf0e7c68db53362efc1d18fb0835
Lukas Boehm <lukas.l.boehm@fau.de>
2025-04-30 12:53:48 +0200

    try testing?

---
+++ test/test_weather.cpp
@@ -0,0 +1,26 @@
+#include <unity.h>
+#include "weather.h"
+
+void setUp(void)
+{
+    // set stuff up here
+}
+
+void tearDown(void)
+{
+    // clean stuff up here
+}
+
+void test_weather_important_fields_present()
+{
+    WeatherData data = fetchWeather(49.4542, 11.0775); // Nürnberg.
+
+    TEST_ASSERT_GREATER_THAN(-30, data.temperature);
+}
+
+int main(int argc, char **argv)
+{
+    UNITY_BEGIN();
+    RUN_TEST(test_weather_important_fields_present);
+    UNITY_END();
+}
