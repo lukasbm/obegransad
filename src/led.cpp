1a279232a6d96e29259bc3e2f5c555414c5a2b37
Lukas Boehm <lukas.l.boehm@fau.de>
2025-05-07 20:27:33 +0200

    work

--- src/led.cpp
+++ src/led.cpp
@@ -3,7 +3,7 @@
 
 void panel_init()
 {
-    pinMode(P_CLA, OUTPUT);
+    pinMode(P_LATCH, OUTPUT);
     pinMode(P_CLK, OUTPUT);
     pinMode(P_DI, OUTPUT);
     pinMode(P_EN, OUTPUT);
@@ -15,16 +15,24 @@ void panel_init()
     ledcWrite(EN_CH, MIN_BRIGHTNESS); // keep blank during init
 }
 
-// helper: shift one bit‑plane
-inline void shiftPlane(uint8_t bit)
+inline void shift_and_latch(uint8_t bit)
 {
-    uint8_t *p = panel_buf;
-    for (uint16_t i = 0; i < 256; i++, p++)
+    noInterrupts();
+    digitalWrite(P_LATCH, LOW); // freeze outputs
+    digitalWrite(P_EN, HIGH);   // OE/ HIGH  (panel dark)
+
+    uint16_t i = 0;
+    for (; i < 256; i++)
     {
-        digitalWrite(P_DI, (*p >> bit) & 1);
+        digitalWrite(P_DI, (panel_buf[i] >> bit) & 1);
         digitalWrite(P_CLK, HIGH);
         digitalWrite(P_CLK, LOW);
     }
+
+    digitalWrite(P_LATCH, HIGH); // transfer SR -> outputs
+    delayMicroseconds(1);        // ≥ 20 ns is enough
+    digitalWrite(P_LATCH, LOW);  // freeze outputs again
+    interrupts();
 }
 
 void panel_setPixel(int8_t row, int8_t col, uint8_t brightness)
@@ -41,10 +49,10 @@ void panel_show()
     for (uint8_t bit = 0; bit < 8; bit++)
     {
         ledcWrite(EN_CH, MIN_BRIGHTNESS); // 1) blank panel while shifting
-        digitalWrite(P_CLA, LOW);         //    keep LA/ LOW so outputs freeze
+        digitalWrite(P_LATCH, LOW);         //    keep LA/ LOW so outputs freeze
         shiftPlane(bit);                  // 2) clock 256 bits of this plane
-        digitalWrite(P_CLA, HIGH);        // 3) short HIGH pulse latches data
-        digitalWrite(P_CLA, LOW);
+        digitalWrite(P_LATCH, HIGH);        // 3) short HIGH pulse latches data
+        digitalWrite(P_LATCH, LOW);
         ledcWrite(EN_CH, MAX_BRIGHTNESS); // 4) enable LEDs
         delayMicroseconds(dur);           // 5) time slice proportional to bit
         dur <<= 1;                        // the higher the bit, the longer the time slice
