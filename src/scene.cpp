bf4f92ddf7019a1a2dc5cb587b6d600d49a210a4
Lukas Boehm <lukas.l.boehm@fau.de>
2025-05-11 11:57:59 +0200

    renames

--- src/scene.cpp
+++ src/scene.cpp
@@ -68,10 +68,10 @@ private:
     void drawNextSprite()
     {
         panel_clear();
-        auto sprite = asset_smallTestSheet.getByIndex(0);
+        auto sprite = sheet_test.getByIndex(0);
         if (sprite != nullptr)
         {
-            panel_drawSprite(2, 2, sprite, asset_smallTestSheet.spriteWidth, asset_smallTestSheet.spriteHeight);
+            panel_drawSprite(2, 2, sprite, sheet_test.spriteWidth, sheet_test.spriteHeight);
         }
         else
         {
@@ -169,17 +169,17 @@ private:
         // minus sign
         if (weatherData.temperature < 0)
         {
-            sprite = asset_thinGlyphSheet.getGlyph(45); // minus sign
-            panel_drawSprite(0, 9, sprite, asset_thinGlyphSheet.spriteWidth, asset_thinGlyphSheet.spriteHeight);
+            sprite = font_thin.getGlyph(45); // minus sign
+            panel_drawSprite(0, 9, sprite, font_thin.spriteWidth, font_thin.spriteHeight);
         }
 
         int temperature = (int)abs(weatherData.temperature);
         // first digit
-        sprite = asset_thinGlyphSheet.getGlyph(temperature / 10 + 48); // temperature/10 is 0-2
-        panel_drawSprite(4, 9, sprite, asset_thinGlyphSheet.spriteWidth, asset_thinGlyphSheet.spriteHeight);
+        sprite = font_thin.getGlyph(temperature / 10 + 48); // temperature/10 is 0-2
+        panel_drawSprite(4, 9, sprite, font_thin.spriteWidth, font_thin.spriteHeight);
         // second digit
-        sprite = asset_thinGlyphSheet.getGlyph(temperature % 10 + 48);
-        panel_drawSprite(9, 9, sprite, asset_thinGlyphSheet.spriteWidth, asset_thinGlyphSheet.spriteHeight);
+        sprite = font_thin.getGlyph(temperature % 10 + 48);
+        panel_drawSprite(9, 9, sprite, font_thin.spriteWidth, font_thin.spriteHeight);
 
         // degree symbol
         panel_setPixel(9, 14, BRIGHTNESS_4);
@@ -187,7 +187,7 @@ private:
         panel_setPixel(10, 14, BRIGHTNESS_4);
         panel_setPixel(10, 15, BRIGHTNESS_4);
 
-        // draw separator
+        // draw separator (1 pixel row in the middle)
         for (int i = 0; i < 16; i++)
         {
             panel_setPixel(8, i, BRIGHTNESS_4);
@@ -196,11 +196,11 @@ private:
         ///////////////// TODO: symbols at top (top 9 pixels)
 
         // cloud
-        sprite = asset_Cloud;
+        sprite = data_cloud;
         panel_drawSprite(0, 0, sprite, 9, 5);
 
         // rain
-        sprite = asset_RainSheet.nextFrame();
+        sprite = animation_rain.nextFrame();
         panel_drawSprite(9, 0, sprite, 7, 8);
     }
 
@@ -247,20 +247,20 @@ private:
         panel_clear();
 
         // hour first digit
-        sprite = asset_boldGlyphSheet.getGlyph((hour / 10) + 48); // hour/10 is 0,1 or 2
-        panel_drawSprite(2, 0, sprite, asset_boldGlyphSheet.spriteWidth, asset_boldGlyphSheet.spriteHeight);
+        sprite = font_bold.getGlyph((hour / 10) + 48); // hour/10 is 0,1 or 2
+        panel_drawSprite(2, 0, sprite, font_bold.spriteWidth, font_bold.spriteHeight);
 
         // hour second digit
-        sprite = asset_boldGlyphSheet.getGlyph((hour % 10) + 48); // hour % 10 is 0-9
-        panel_drawSprite(9, 0, sprite, asset_boldGlyphSheet.spriteWidth, asset_boldGlyphSheet.spriteHeight);
+        sprite = font_bold.getGlyph((hour % 10) + 48); // hour % 10 is 0-9
+        panel_drawSprite(9, 0, sprite, font_bold.spriteWidth, font_bold.spriteHeight);
 
         // minute first digit
-        sprite = asset_boldGlyphSheet.getGlyph((minute / 10) + 48); // minute/10 is 0-5
-        panel_drawSprite(2, 9, sprite, asset_boldGlyphSheet.spriteWidth, asset_boldGlyphSheet.spriteHeight);
+        sprite = font_bold.getGlyph((minute / 10) + 48); // minute/10 is 0-5
+        panel_drawSprite(2, 9, sprite, font_bold.spriteWidth, font_bold.spriteHeight);
 
         // minute second digit
-        sprite = asset_boldGlyphSheet.getGlyph((minute % 10) + 48); // minute % 10 is 0-9
-        panel_drawSprite(9, 9, sprite, asset_boldGlyphSheet.spriteWidth, asset_boldGlyphSheet.spriteHeight);
+        sprite = font_bold.getGlyph((minute % 10) + 48); // minute % 10 is 0-9
+        panel_drawSprite(9, 9, sprite, font_bold.spriteWidth, font_bold.spriteHeight);
     }
 
 public:
