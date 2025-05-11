30eef3a86ef752c019adc26f2f457468be8dea75
Lukas Boehm <lukas.l.boehm@fau.de>
2025-05-11 11:55:37 +0200

    add moon asset

--- assets/convert_all.sh
+++ assets/convert_all.sh
@@ -6,8 +6,8 @@ out_dir="../src/sprites"
 # python3 img2cpp.py sheet BoldGlyphs6x7.bmp 6 7 > "${out_dir}/bold_glyphs.hpp"
 
 # python3 img2cpp.py sheet Sun.bmp 9 9 > "${out_dir}/sun.hpp"
-# python3 img2cpp.py single Moon.bmp > "${out_dir}/moon.hpp"
 # python3 img2cpp.py sheet Rain.bmp 7 8  > "${out_dir}/rain.hpp"
 # python3 img2cpp.py single Cloud.bmp > "${out_dir}/cloud.hpp"
+python3 img2cpp.py sheet MoonSmall.bmp 9 9 > "${out_dir}/moon_small.hpp"
 
 python3 img2cpp.py single WiFi.bmp > "${out_dir}/wifi.hpp"
