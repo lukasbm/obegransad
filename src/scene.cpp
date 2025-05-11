f33b3dd38075bac52009fba0d09079995dffa737
Lukas Boehm <lukas.l.boehm@fau.de>
2025-05-11 02:26:06 +0200

    new scene

--- src/scene.cpp
+++ src/scene.cpp
@@ -6,6 +6,11 @@
 #include "weather.h"
 #include "clock.h"
 #include "sprites/small_test.hpp"
+#include "sprites/bold_glyphs.hpp"
+#include "sprites/thin_glyphs.hpp"
+#include "sprites/cloud.hpp"
+#include "sprites/sun.hpp"
+#include "sprites/rain.hpp"
 
 class Scene
 {
@@ -152,70 +157,121 @@ class WeatherScene : public Scene
 {
 private:
     struct WeatherData weatherData;
-    int secondsSinceLastFetch = 0;
-    int lastSecond = -1;
 
     void drawWeatherData()
     {
-        // TODO: need sprites first!
+        panel_clear();
+
+        const uint8_t *sprite;
+
+        ////////////////// temperature at the bottom (bottom 6 pixels)
+
+        // first digit
+        if (weatherData.temperature < 0)
+        {
+            sprite = asset_boldGlyphSheet.getGlyph(45); // minus sign
+            panel_drawSprite(0, 9, sprite, asset_boldGlyphSheet.spriteWidth, asset_boldGlyphSheet.spriteHeight);
+        }
+        // first digit
+        sprite = asset_boldGlyphSheet.getGlyph(weatherData.temperature / 10 + 48); // temperature/10 is 0-2
+        panel_drawSprite(4, 9, sprite, asset_boldGlyphSheet.spriteWidth, asset_boldGlyphSheet.spriteHeight);
+
+        // second digit
+        sprite = asset_boldGlyphSheet.getGlyph(weatherData.temperature / 10 + 48); // temperature/10 is 0,1 or 2
+        panel_drawSprite(9, 9, sprite, asset_boldGlyphSheet.spriteWidth, asset_boldGlyphSheet.spriteHeight);
+
+        // degree symbol
+        panel_setPixel(14, 9, BRIGHTNESS_4);
+        panel_setPixel(15, 9, BRIGHTNESS_4);
+        panel_setPixel(14, 10, BRIGHTNESS_4);
+        panel_setPixel(15, 10, BRIGHTNESS_4);
+
+        ///////////////// TODO: symbols at top (top 9 pixels)
+
+        // cloud
+        sprite = asset_Cloud;
+        panel_drawSprite(0, 0, sprite, 9, 5);
+
+        // rain
+        sprite = asset_RainSheet.nextFrame();
+        panel_drawSprite(0, 9, sprite, 7, 8);
     }
 
 public:
     void activate() override
     {
         Serial.println("Weather scene activated");
+        // initial data
         weatherData = fetchWeather(settings.weather_latitude, settings.weather_longitude);
+        drawWeatherData();
     }
 
     void update() override
     {
-        int currSecond = time_second();
+        static unsigned long lastFetch = millis();
+        static unsigned long lastDraw = 0;
 
-        if (time_second() != lastSecond && secondsSinceLastFetch >= 65)
+        const int weatherUpdateInterval = 600; // 10 minutes
+
+        unsigned long now = millis();
+        if (now - lastFetch > weatherUpdateInterval * 1000)
         {
-            secondsSinceLastFetch++;
-            if (secondsSinceLastFetch >= settings.weather_update_interval)
-            {
-                weatherData = fetchWeather(settings.weather_latitude, settings.weather_longitude);
-                secondsSinceLastFetch = 0;
-            }
+            weatherData = fetchWeather(settings.weather_latitude, settings.weather_longitude);
+            lastFetch = now;
+        }
+        // have to draw all the time (every second) because of animations
+        if (now - lastDraw > 1000)
+        {
+            lastDraw = now;
             drawWeatherData();
         }
-
-        lastSecond = currSecond;
     }
 };
 
 class ClockScene : public Scene
 {
 private:
-    int lastMinute = -1;
-
     void drawTime(uint8_t hour, uint8_t minute)
     {
-        // panel_printChar(2, 0, (hour / 10) + 48);
-        // panel_printChar(9, 0, (hour % 10) + 48);
-        // panel_printChar(2, 9, (minute / 10) + 48);
-        // panel_printChar(9, 9, (minute % 10) + 48);
+        const uint8_t *sprite;
+
+        panel_clear();
+
+        // hour first digit
+        sprite = asset_boldGlyphSheet.getGlyph((hour / 10) + 48); // hour/10 is 0,1 or 2
+        panel_drawSprite(2, 0, sprite, asset_boldGlyphSheet.spriteWidth, asset_boldGlyphSheet.spriteHeight);
+
+        // hour second digit
+        sprite = asset_boldGlyphSheet.getGlyph((hour % 10) + 48); // hour % 10 is 0-9
+        panel_drawSprite(9, 0, sprite, asset_boldGlyphSheet.spriteWidth, asset_boldGlyphSheet.spriteHeight);
+
+        // minute first digit
+        sprite = asset_boldGlyphSheet.getGlyph((minute / 10) + 48); // minute/10 is 0-5
+        panel_drawSprite(2, 9, sprite, asset_boldGlyphSheet.spriteWidth, asset_boldGlyphSheet.spriteHeight);
+
+        // minute second digit
+        sprite = asset_boldGlyphSheet.getGlyph((minute % 10) + 48); // minute % 10 is 0-9
+        panel_drawSprite(9, 9, sprite, asset_boldGlyphSheet.spriteWidth, asset_boldGlyphSheet.spriteHeight);
     }
 
 public:
     void activate() override
     {
         Serial.println("Clock scene activated");
-        drawTime(time_hour(), time_minute());
+        struct tm time = time_fetch();
+        drawTime(time.tm_hour, time.tm_hour);
     }
 
     void update() override
     {
-        int currMinute = time_minute();
+        static int lastMinute = -1;
 
-        if (currMinute != lastMinute)
+        struct tm time = time_fetch();
+        if (time.tm_min != lastMinute)
         {
-            drawTime(time_hour(), currMinute);
+            drawTime(time.tm_hour, time.tm_hour);
+            lastMinute = time.tm_min;
         }
-
-        lastMinute = currMinute;
     }
 };
 
