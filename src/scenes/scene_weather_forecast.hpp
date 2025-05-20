f0846f7a12ca78d837b1d74a55f8bd67a9c95d23
Lukas Boehm <lukas.l.boehm@fau.de>
2025-05-20 12:11:27 +0200

    a

---
+++ src/scenes/scene_weather_forecast.hpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+#include "scene.h"
+#include "weather.h"
+#include "sprites/thin_glyphs.hpp"
+
+// regular temp at top, then a line for the next 7 days forcast. temperature relative to current.
+class WeatherForecast : public Scene
+{
+public:
+    void activate() override
+    {
+    }
+
+    void update() override
+    {
+    }
+};
---
+++ src/scenes/scene_weather_minmax.hpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+#include "scene.h"
+#include "weather.h"
+#include "sprites/thin_glyphs.hpp"
+
+// a line for the next 7 days forcast. temperature relative to current.
+class WeatherMinMax : public Scene
+{
+public:
+    void activate() override
+    {
+    }
+
+    void update() override
+    {
+    }
+};
