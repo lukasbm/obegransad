94b3d57d4a34317f81f89f2ef240eba5ca78a33b
Lukas Boehm <lukas.l.boehm@fau.de>
2025-07-07 00:33:30 +0200

    aa

--- components/ikea-obegransad-panel/include/ikea-obegransad-panel.h
+++ components/ikea-obegransad-panel/include/ikea-obegransad-panel.h
@@ -1,32 +1,48 @@
 #pragma once
 
 #include <esp_err.h>
+#include <stdint.h>
 
 #ifdef __cplusplus
-extern "C"
-{
+extern "C" {
 #endif
 
-#define PANEL_WIDTH 16
-#define PANEL_HEIGHT 16
-#define BIT_DEPTH 2
+extern uint8_t gBright; // global brightness (0-255) (only scaling)
 
-    typedef struct
-    {
-        int latch_pin;
-        int clk_pin;
-        int di_pin;
-        int oe_pin;
-        int spi_host;
-        int spi_clock_speed_hz;
-        int rmt_channel;
-        int timer_group;
-        int timer_idx;
-    } panel_config_t;
+enum Brightness {
+  PANEL_BRIGHTNESS_OFF,
+  PANEL_BRIGHTNESS_1,
+  PANEL_BRIGHTNESS_2,
+  PANEL_BRIGHTNESS_3,
+};
 
-    esp_err_t panel_init(const panel_config_t *config);
-    void panel_commit(void);
-    uint8_t *panel_get_framebuffer(void);
+typedef struct {
+  int latch_pin;
+  int clk_pin;
+  int di_pin;
+  int oe_pin;
+  int spi_host;
+  int spi_clock_speed_hz;
+  int rmt_channel;
+  int timer_group;
+  int timer_idx;
+} panel_config_t;
+
+esp_err_t panel_init(const panel_config_t *config);
+
+void panel_timer_start(void);
+
+void panel_timer_stop(void);
+
+void panel_setPixel(uint8_t row, uint8_t col, Brightness brightness);
+
+void panel_fill(Brightness col);
+
+inline void panel_clear() { panel_fill(PANEL_BRIGHTNESS_OFF); }
+
+uint8_t *panel_get_framebuffer(void);
+
+void panel_commit(void);
 
 #ifdef __cplusplus
 }
--- components/ikea-obegransad-panel/test/test_panel.cpp
+++
@@ -1,6 +0,0 @@
-#include <unity.h>
-
-TEST_CASE("my display driver initializes", "[panel]")
-{
-    
-}
