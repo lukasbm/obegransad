7a7596e43954614f4b59a23d7de07102c35c9ce1
Lukas Boehm <lukas.l.boehm@fau.de>
2025-07-13 18:56:53 +0200

    fix: enhance error logging for mbedtls failures and handle HTTP redirects

--- main/weather.cpp
+++ main/weather.cpp
@@ -78,7 +78,7 @@ esp_err_t event_handler(esp_http_client_event_t *evt) {
     }
     break;
 
-  case HTTP_EVENT_DISCONNECTED:
+  case HTTP_EVENT_DISCONNECTED: {
     ESP_LOGI(TAG, "HTTP_EVENT_DISCONNECTED");
     int mbedtls_err = 0;
     esp_err_t err = esp_tls_get_and_clear_last_error(
@@ -88,11 +88,14 @@ esp_err_t event_handler(esp_http_client_event_t *evt) {
       ESP_LOGI(TAG, "Last mbedtls failure: 0x%x", mbedtls_err);
     }
     break;
+  }
 
-  case HTTP_EVENT_REDIRECT:
+  case HTTP_EVENT_REDIRECT: {
     ESP_LOGD(TAG, "HTTP_EVENT_REDIRECT");
     esp_http_client_set_redirection(evt->client);
     break;
+  }
+
   }
   return ESP_OK;
 }
