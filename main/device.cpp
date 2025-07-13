905d6394d31615617136d12088034958d3d497d7
Lukas Boehm <lukas.l.boehm@fau.de>
2025-07-13 23:56:50 +0200

    :fire: remove littleFS

--- main/device.cpp
+++ main/device.cpp
@@ -5,7 +5,6 @@
 #include "esp_err.h"   // for esp_err_t and ESP_ERROR_CHECK
 #include "esp_event.h" // for esp_event_loop_create_default()
 #include "nvs_flash.h" // for nvs_flash_init()
-#include <esp_littlefs.h>
 #include <esp_log.h>
 #include <esp_sleep.h>
 #include <esp_system.h>
@@ -16,31 +15,6 @@
 #include "wifi_station.h"          // WifiStation::GetInstance()
 
 static const char *TAG = "device";
-
-static esp_err_t littlefs_init() {
-  esp_vfs_littlefs_conf_t conf = {};
-  conf.base_path = "/littlefs";
-  conf.partition_label = "storage";
-  conf.format_if_mount_failed = true;
-  conf.dont_mount = false;
-
-  // Use settings defined above to initialize and mount LittleFS filesystem.
-  // Note: esp_vfs_littlefs_register is an all-in-one convenience function.
-  ESP_RETURN_ON_ERROR(esp_vfs_littlefs_register(&conf), TAG,
-                      "Failed to register LittleFS");
-
-  size_t total = 0, used = 0;
-  esp_err_t ret = esp_littlefs_info(conf.partition_label, &total, &used);
-  if (ret != ESP_OK) {
-    ESP_LOGE(TAG, "Failed to get LittleFS partition information (%s)",
-             esp_err_to_name(ret));
-    esp_littlefs_format(conf.partition_label);
-  } else {
-    ESP_LOGI(TAG, "Partition size: total: %d, used: %d", total, used);
-  }
-
-  return ESP_OK;
-}
 
 esp_err_t device_init() {
   // network stack
@@ -59,9 +33,6 @@ esp_err_t device_init() {
     ret = nvs_flash_init();
   }
   ESP_RETURN_ON_ERROR(ret, TAG, "Failed to initialize NVS flash");
-
-  // Little FS
-  ESP_RETURN_ON_ERROR(littlefs_init(), TAG, "Failed to initialize LittleFS");
 
   return ESP_OK;
 }
